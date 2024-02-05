3b2cd6490dfcc28d912fcaf9bc9e2be3535b2432
Renzo Maggiori <renzo.maggiori@epitech.eu>
2024-02-05 10:45:51 +0100

    [add] IComponent, AComponent, Link && special components

---
+++ special/FalseComponent.cpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** nanoTek
+** File description:
+** FalseComponent
+*/
+
+#include "FalseComponent.hpp"
+
+nts::FalseComponent::FalseComponent() {
+    std::shared_ptr<nts::Tristate> status = std::make_shared<nts::Tristate>(Tristate::False);
+    this->getPins()[1] = status;
+}
+
+nts::pinType nts::FalseComponent::getPinType(std::size_t pin) {
+    if (pin != 1) throw Error("Invalid pin.");
+    return pinType::OUTPUT;
+}
+
+void nts::FalseComponent::updateOutputPin() {
+    return;
+}
---
+++ special/InputComponent.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** nanoTek
+** File description:
+** InputComponent
+*/
+
+#ifndef INPUTCOMPONENT_HPP_
+#define INPUTCOMPONENT_HPP_
+
+#include "../AComponent.hpp"
+
+namespace nts {
+    class InputComponent: public AComponent {
+        public:
+            InputComponent();
+            ~InputComponent();
+
+        protected:
+        private:
+    };
+}
+
+#endif
