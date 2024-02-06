85a9a53987a65cff08de185fce0c76e99ca3497c
Gonzalo <gonzalo.larroya-martinez@epitech.eu>
2024-02-06 14:45:23 +0100

    [fix] chipset 4069

--- components/chipsets/Chipset4069.cpp
+++ components/chipsets/Chipset4069.cpp
@@ -16,6 +16,8 @@ nts::Chipset4069::Chipset4069()
     for (int i = 1; i < 7; i++) {
         this->_components[i] = std::make_unique<nts::NotComponent>();
         AComponent *derivedComponent = dynamic_cast<AComponent *>(this->_components[i].get());
+        if (i == 4)
+            y++;
         for (int count = 1; count < 3; count++) {
             this->_pins[y] = derivedComponent->getPin(count);
             y++;
