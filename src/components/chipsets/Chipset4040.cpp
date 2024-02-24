8fbb24b8db97c593d8bcbfdf5bc9539cec90cb1f
Gonzalo <gonzalo.larroya-martinez@epitech.eu>
2024-02-24 14:03:52 +0100

    [fix] chipset 4040 error

--- src/components/chipsets/Chipset4040.cpp
+++ src/components/chipsets/Chipset4040.cpp
@@ -14,19 +14,19 @@ nts::Chipset4040::Chipset4040()
 
     this->_pins[10] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[10];
     this->_pins[11] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[11];
-    
+
     this->_pins[1] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[1];
-    this->_pins[2] = static_cast<CounterComponent*>(this->_components[2].get())->getPins()[2];
-    this->_pins[3] = static_cast<CounterComponent*>(this->_components[2].get())->getPins()[3];
-    this->_pins[4] = static_cast<CounterComponent*>(this->_components[2].get())->getPins()[4];
-    this->_pins[5] = static_cast<CounterComponent*>(this->_components[3].get())->getPins()[5];
-    this->_pins[6] = static_cast<CounterComponent*>(this->_components[3].get())->getPins()[6];
-    this->_pins[7] = static_cast<CounterComponent*>(this->_components[3].get())->getPins()[7];
-    this->_pins[9] = static_cast<CounterComponent*>(this->_components[4].get())->getPins()[9];
-    this->_pins[12] = static_cast<CounterComponent*>(this->_components[4].get())->getPins()[12];
-    this->_pins[13] = static_cast<CounterComponent*>(this->_components[4].get())->getPins()[13];
-    this->_pins[14] = static_cast<CounterComponent*>(this->_components[4].get())->getPins()[14];
-    this->_pins[15] = static_cast<CounterComponent*>(this->_components[4].get())->getPins()[15];
+    this->_pins[1] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[2];
+    this->_pins[3] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[3];
+    this->_pins[4] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[4];
+    this->_pins[5] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[5];
+    this->_pins[6] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[6];
+    this->_pins[7] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[7];
+    this->_pins[9] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[9];
+    this->_pins[12] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[12];
+    this->_pins[13] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[13];
+    this->_pins[14] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[14];
+    this->_pins[15] = static_cast<CounterComponent*>(this->_components[1].get())->getPins()[15];
     this->_pins[8] = std::make_pair<std::shared_ptr<nts::Tristate>, nts::pinType>(nullptr, nts::pinType::NONE);;
     this->_pins[16] = std::make_pair<std::shared_ptr<nts::Tristate>, nts::pinType>(nullptr, nts::pinType::NONE);
 }
